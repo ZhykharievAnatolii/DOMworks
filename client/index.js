8d469d3cf1be893b9b546d66bce44a9d57ef79f3
ZhykharievAnatolii <nekijt87@gmail.com>
2023-01-07 12:53:06 +0200

    upd

--- client/index.js
+++ client/index.js
@@ -3,3 +3,24 @@
 const listItem=document.querySelector('header ul a ');
 
 // document.querySelectorAll()- мертвая коллекция в ней есть forEach
+// document.getElementById()- возвращает один елемент
+// document.getElementsByClassName()
+// document.getElementsByName()
+// document.getElementsByTagName()- возвращают живые коллекции
+
+// listItem.children- коллекция детей
+// listItem.firstElementChild- первый
+// listItem.lastElementChild- последний ребенок елемента
+// listItem.nextElementSibling-
+// listItem.previousElementSibling
+// listItem.parentElement
+// listItem.children[2]
+
+listItem.id
+
+const link= listItem.firstElementChild;
+link.href;
+
+link.classList
+link.classList.add('first', 'last');
+link.classList.remove('first')
