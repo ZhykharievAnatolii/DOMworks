3e42aad251ec1f6fc4ddff21cc1a2f18940ecb67
ZhykharievAnatolii <nekijt87@gmail.com>
2023-01-07 18:51:48 +0200

    upd js

--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 // методы для поиска DOM-елементов
 
-const listItem=document.querySelector('header ul a ');
+const listItem=document.querySelector(' ');
 
 // document.querySelectorAll()- мертвая коллекция в ней есть forEach
 // document.getElementById()- возвращает один елемент
@@ -16,35 +16,43 @@ const listItem=document.querySelector('header ul a ');
 // listItem.parentElement
 // listItem.children[2]
 
-listItem.id
+// listItem.id
 
-const link= listItem.firstElementChild;
-link.href;
-
-link.classList
-link.classList.add('first', 'last');
-link.classList.remove('first');
-// link.classList.toggle('test')=== добавляет класс если не было, уделяет если был
-link.classList.contains('test');
-
-link.getAttribute('test');
-link.setAttributeNode();
-link.removeAttribute();
-// link.hasAttribute();- проверяет наличие
-
-
-link.dataset.action='test';
-link.dataset.action1='test1';
-delete link.dataset.action1
-
-link.closest('.container');
-link.matches('a');
+// const link= listItem.firstElementChild;
+// link.href;
+//
+// link.classList
+// link.classList.add('first', 'last');
+// link.classList.remove('first');
+// // link.classList.toggle('test')=== добавляет класс если не было, уделяет если был
+// link.classList.contains('test');
+//
+// link.getAttribute('test');
+// link.setAttributeNode();
+// link.removeAttribute();
+// // link.hasAttribute();- проверяет наличие
+//
+//
+// link.dataset.action='test';
+// link.dataset.action1='test1';
+// delete link.dataset.action1
+//
+// link.closest('.container');
+// link.matches('a');
 
 const newItem=document.createElement('li');
 newItem.innerHTML=`
 <span>Logo</span>
 <figure><img src="" alt="">
-<figcaption></figcaption>
+<figcaption>
+Caption
+</figcaption>
 </figure>
-`
+`;
+listItem.parentElement.append(newItem);
+listItem.replaceWith(newItem);
+newItem.parentElement.append(listItem);
+
+// about events
+
 
