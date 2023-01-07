2f030cdf13f58f05e73841c4834a5a0bb521f594
ZhykharievAnatolii <nekijt87@gmail.com>
2023-01-07 20:47:24 +0200

    upd js about promises

--- client/index.js
+++ client/index.js
@@ -18,7 +18,7 @@ const listItem=document.querySelector('header');
 
 // listItem.id
 
-// const link= listItem.firstElementChild;
+const link= listItem.firstElementChild;
 // link.href;
 //
 // link.classList
@@ -59,6 +59,71 @@ listItem.parentElement.append(newItem);
 // addEventListener()- можно добавть сразу несколько обработчиков
 // Можно также прописать в теге html
 
+// link.onclick=()=>{
+//     console.log('click')
+// }
+//
+// console.log('begin');
+//
+//
+//
+// console.log('end');
+
+// Promice- это возможность уйти от коллбека
+// Они вводят дополнительную очередь выполнения задач- микротаски
+// Для того, чтобы что-то приоритетнее выполнить, обогнать макротаски
+// Сам промис не является микрозадачей, он елемент синхронного кода
+// В микротаски попадают обработчики зенов и кечей
+// Для выполнения должен быть триггер- resolve или reject
+// если промис записан просто в коде, то он сразу выполнится, поэтому его заворачиваем в функцию
+
+
+// console.log('begin');
+// setTimeout(()=>{
+//     console.log('timeout')
+// },0);
+// function timer(time){
+//     console.log('before promice')
+//     return new Promise((resolve, reject)=>{
+//         console.log('inside promice before timeout');
+//         setTimeout(()=>{
+//             console.log('inside promice in timeout');
+//             resolve();
+//         },time);
+//         console.log('inside promice after timeout')
+//     })
+// };
+// timer(1000).then(()=>{
+//     console.log('then')
+// }).catch(err=>{
+//     console.log('cath')
+// })
+// console.log('end');
+function timer(time){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            resolve();
+        },time);
+    })
+};
+timer(1000).then(()=>{
+    console.log('then');
+    Promise.resolve().then(()=>{
+        console.log('inner then')
+    });
+    throw new Error();
+}).catch(err=>{
+    console.log('cath')
+});
+console.log('end');
+timer();
+
+// function resolver() {
+//     Promise.resolve().then(()=>{
+//         resolver()
+//     })
+//
+// }--- в таком случае перестанут работать обработчики, постоянно по кругу будут микрозадачи попадать в стек
 
 
 
